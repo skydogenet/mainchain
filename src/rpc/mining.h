bfe9506883d092c52a24cc4c50a07a68c0cf2549
www <skydogenet@protonmail.com>
2023-05-14 01:47:21 -0400

    BIP 300

--- src/rpc/mining.h
+++ src/rpc/mining.h
@@ -25,44 +25,6 @@ namespace Consensus { struct Params; };
 
 
 
-private:
-    // utility functions
-    /** Clear the block's state and prepare for assembling a new block */
-    void resetBlock();
-    /** Add a tx to the block */
-    void AddToBlock(CTxMemPool::txiter iter);
-
-    // Methods for how to add transactions to a block.
-    /** Add transactions based on feerate including unconfirmed ancestors
-      * Increments nPackagesSelected / nDescendantsUpdated with corresponding
-      * statistics from the package selection (for logging statistics). */
-    void addPackageTxs(int &nPackagesSelected, int &nDescendantsUpdated, bool fDrivechainEnabled, bool& fNeedCriticalFeeTx, const std::set<uint8_t>& setSidechainsWithWithdrawal);
-
-    // helper functions for addPackageTxs()
-    /** Remove confirmed (inBlock) entries from given set */
-    void onlyUnconfirmed(CTxMemPool::setEntries& testSet);
-    /** Test if a new package would "fit" in the block */
-    bool TestPackage(uint64_t packageSize, int64_t packageSigOpsCost) const;
-    /** Perform checks on each transaction in a package:
-      * locktime, premature-witness, serialized size (if necessary)
-      * These checks should always succeed, and they're here
-      * only as an extra check in case of suboptimal node configuration */
-    bool TestPackageTransactions(const CTxMemPool::setEntries& package);
-    /** Return true if given transaction from mapTx has already been evaluated,
-      * or if the transaction's cached data in mapTx is incorrect. */
-    bool SkipMapTxEntry(CTxMemPool::txiter it, indexed_modified_transaction_set &mapModifiedTx, CTxMemPool::setEntries &failedTx);
-    /** Sort the package in an order that is valid to appear in a block */
-    void SortForBlock(const CTxMemPool::setEntries& package, CTxMemPool::txiter entry, std::vector<CTxMemPool::txiter>& sortedEntries);
-    /** Add descendants of given transactions to mapModifiedTx with ancestor
-      * state updated assuming given transactions are inBlock. Returns number
-      * of updated descendants. */
-    int UpdatePackagesForAdded(const CTxMemPool::setEntries& alreadyAdded, indexed_modified_transaction_set &mapModifiedTx);
-
-    // SidechainDB
-    /** Create withdrawal payout transaction for nSidechain if needed */
-    bool CreateWithdrawalPayout(uint8_t nSidechain, CMutableTransaction& tx, CAmount& nFees);
-};
-
 /** Miner functions restored from Bitcoin 0.12 */
 
 /** Run the miner threads */
